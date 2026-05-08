170f25f21c5bfaafac31cf30bad3b0059eb100c0
elibe15 <elybrito2007@gmail.com>
2026-05-08 17:30:20 -0400

    organización corrección
    se corrigió la organización de los archivos para que funcione en netlify

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [
         tailwindcss(),
-    ]
+    ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                historia: resolve(__dirname, 'pages/historia.html'),
+                galeria: resolve(__dirname, 'pages/galeria.html'),
+                inscripcion: resolve(__dirname, 'pages/inscripcion.html'),
+                nosotros: resolve(__dirname, 'pages/nosotros.html'),
+            },
+        },
+    },
 })
